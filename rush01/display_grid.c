495a691b5d1b53935a6bd1a5caff9682863eb85c
Leonard French <ljfrench@gmail.com>
2023-12-01 22:28:26 +0100

    Ideas for rush01 project

---
+++ rush01/display_grid.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_grid.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrench <lfrench@student.42luxembourg.lu>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 21:43:22 by lfrench           #+#    #+#             */
+/*   Updated: 2023/12/01 22:16:07 by lfrench          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Learning to display a grid */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main(int argc, **argv)
+{
+	
+}
+
+void	*parse_numbers(const char *str)
+{
+	int	i;
+
+	while (i != '/0')
+	{
+		if (str[i] == ' ')
+			i++;
+	
+}
+	
