0590459f190600301a32664f7aba3067ef9ae883
wheagle <ljfrench@gmail.com>
2023-12-06 14:37:16 +0100

    Create ft_strcat.c

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 13:46:51 by lfrench           #+#    #+#             */
+/*   Updated: 2023/12/06 14:02:15 by lfrench          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src);
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != 0)
+		i++;
+	j = 0;
+	while (src[j] != 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
