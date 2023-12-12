ec0af781cdef8c0e965a4194849178adec49fe24
wheagle <ljfrench@gmail.com>
2023-12-12 14:18:52 +0100

    Update ft_strcat.c

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -6,9 +6,14 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:46:51 by lfrench           #+#    #+#             */
-/*   Updated: 2023/12/06 14:02:15 by lfrench          ###   ########.fr       */
+/*   Updated: 2023/12/08 12:19:47 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* The  strcat()  function appends the src string to the dest string, 
+   overwriting the terminating null byte ('\0') at the end of dest, 
+   and then adds a terminating null byte. The strings may not overlap, 
+   and the dest string must have enough space for the result. */
 
 char	*ft_strcat(char *dest, char *src);
 
@@ -30,3 +35,15 @@ char	*ft_strcat(char *dest, char *src)
 	dest[i] = 0;
 	return (dest);
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	src[] = "source";
+	char	dest[12] = "dest";
+
+	printf("\"%s\" appended to \"%s\" is:\n", src, dest);
+	printf("%s\n", ft_strcat(dest, src));
+
+	return (0);
+}*/
