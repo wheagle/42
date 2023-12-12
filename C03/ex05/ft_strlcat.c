f3c5e1434b59d8a2a7539a94e39e9507928216fa
wheagle <ljfrench@gmail.com>
2023-12-12 14:20:15 +0100

    Update ft_strlcat.c

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,27 +6,76 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:46:51 by lfrench           #+#    #+#             */
-/*   Updated: 2023/12/06 14:30:18 by lfrench          ###   ########.fr       */
+/*   Updated: 2023/12/11 13:13:00 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	*ft_strlcat(char *dest, char *src, unsigned int nb);
+/* The strlcpy() and strlcat() functions copy and concatenate strings 
+   respectively.  They are designed to be safer, more consistent, and less 
+   error prone replacements for strncpy(3) and strncat(3). Unlike those 
+   functions, strlcpy() and strlcat() take the full size of the buffer (not
+   just the length) and guarantee to NUL-terminate the result (as long as 
+   size is larger than 0 or, in the case of strlcat(), as long as there is 
+   at least one byte free in dst).  Note that a byte for the NUL should be 
+   included in size.  Also note that strlcpy() and strlcat() only operate on 
+   true “C” strings.  This means that for strlcpy() src must be NUL-terminated 
+   and for strlcat() both src and dst must be NUL-terminated.
+   The strlcpy() function copies up to size - 1 characters from the NUL-
+   terminated string src to dst, NUL-terminating the result.
+   The strlcat() function appends the NUL-terminated string src to the end of 
+   dst. It will append at most size - strlen(dst) - 1 bytes, NUL-terminating 
+   the result. 
+   The strlcpy() and strlcat() functions return the total length of the string
+   they tried to create.  For strlcpy() that means the length of src.  
+   For strlcat() that means the initial length of dst plus the length of src.
+   While this may seem somewhat confusing, it was done to make truncation
+   detection simple.
+   Note, however, that if strlcat() traverses size characters without finding 
+   a NUL, the length of the string is considered to be size and the destination 
+   string will not be NUL-terminated (since there was no space for the NUL).  
+   This keeps strlcat() from running off the end of a string.  In practice this
+   should not happen (as it means that either size is incorrect or that dst is 
+   not a proper “C” string).  The check exists to prevent potential security
+   problems in incorrect code.*/
 
-unsigned int	*ft_strlcat(char *dest, char *src, unsigned int nb);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
-	int	j;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	j;
 
-	i = 0;
-	while (dest[i] != 0)
-		i++;
+	dest_len = 0;
+	src_len = 0;
+	while (dest[dest_len] != 0)
+		dest_len++;
+	while (src[src_len] != 0)
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
 	j = 0;
-	while (j< nb && src[j] != 0)
+	while (j < size - dest_len - 1 && src[j] != 0)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[dest_len + j] = src[j];
 		j++;
 	}
-	dest[i] = 0;
-	return (dest);
+	dest[dest_len + j] = 0;
+	return (dest_len + src_len);
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int		size = 9;
+	char	src[] = "source";
+	char	dest[12] = "dest";
+	int		cat_len = ft_strlcat(dest, src, size);
+
+	printf("\"%s\" appended to \"%s\"", src, dest);
+	printf(" limited by size %u is:\n", size);
+	printf("%s\n", dest);
+	printf("Return is: %u\n", cat_len);
+
+	return (0);
+}*/
