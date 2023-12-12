f5e9197925c2347fab6b01d0ff6caf796d6af0bf
wheagle <ljfrench@gmail.com>
2023-12-12 14:18:27 +0100

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,15 +6,25 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:53:14 by lfrench           #+#    #+#             */
-/*   Updated: 2023/12/06 13:45:14 by lfrench          ###   ########.fr       */
+/*   Updated: 2023/12/12 14:16:39 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* The strcmp() function compares the two strings s1 and s2. The locale is not 
+   taken into account (for a locale-aware comparison, see strcoll(3)).  
+   The comparison is done using unsigned characters. strcmp() returns 
+   an integer indicating the result of the comparison, as follows:
+   • 0, if the s1 and s2 are equal;
+   • a negative value if s1 is less than s2;
+   • a positive value if s1 is greater than s2.
+   The  strncmp() function is similar, except it compares only 
+   the first (at most) n bytes of s1 and s2. */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < n && (s1[i] != 0 || s2[i] != 0))
@@ -34,9 +44,9 @@ int	main(void)
 	char	str3[] = "Helln";
 	char	str4[] = "World";
 
-	printf("%s and %s result: %d\n", str1, str2, ft_strcmp(str1, str2));
-	printf("%s and %s result: %d\n", str1, str3, ft_strcmp(str1, str3));
-	printf("%s and %s result: %d\n", str1, str4, ft_strcmp(str1, str4));
+	printf("%s+%s, size %u: %d\n", str1, str2, 3, ft_strncmp(str1, str2, 3));
+	printf("%s+%s, size %u: %d\n", str1, str3, 5, ft_strncmp(str1, str3, 5));
+	printf("%s+%s, size %u: %d\n", str1, str4, 3, ft_strncmp(str1, str4, 3));
 
 	return (0);
 }*/
