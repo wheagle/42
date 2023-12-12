b59df5fc707540c376d2223543299c92e804cf8f
wheagle <ljfrench@gmail.com>
2023-12-12 14:17:57 +0100

    Update ft_strcmp.c

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,15 +6,24 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:53:14 by lfrench           #+#    #+#             */
-/*   Updated: 2023/12/06 12:19:10 by lfrench          ###   ########.fr       */
+/*   Updated: 2023/12/08 12:07:40 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* The strcmp() function compares the two strings s1 and s2. The locale is not 
+   taken into account (for a locale-aware comparison, see strcoll(3)).  
+   The comparison is done using unsigned characters.
+   strcmp() returns an integer indicating the result of the comparison, 
+   as follows:
+   • 0, if the s1 and s2 are equal;
+   • a negative value if s1 is less than s2;
+   • a positive value if s1 is greater than s2. */
 
 int	ft_strcmp(char *s1, char *s2);
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int				i;
+	int	i;
 
 	i = 0;
 	while (s1[i] != 0 || s2[i] != 0)
