17925d8cc0ed8a977d70f5a95d5ebace17a0268a
Leonard French <lfrench@c1r7s2.42luxembourg.lu>
2024-02-23 13:55:43 +0100

    Put libft.h prototypes in alphabetical order for ease of reading/verifying.

--- Core/libft/libft.h
+++ Core/libft/libft.h
@@ -6,19 +6,19 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:37:28 by lfrench           #+#    #+#             */
-/*   Updated: 2024/02/21 18:13:09 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/02/23 13:55:04 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isalpha(int c);
 int	ft_isascii(int c);
+int	ft_isdigit(int c);
 int	ft_isprint(int c);
-int	ft_toupper(int c);
 int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 #endif
