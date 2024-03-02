1e29f0999f9f081948e203e60bb6549dfe289726
wheagle <ljfrench@gmail.com>
2024-03-02 17:03:14 +0100

    Create list_mgmt_notes.c
    Just some sample code collected in one place.

---
+++ list_mgmt_notes.c
@@ -0,0 +1,32 @@
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void insertAtBeginning(int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = head;
+    head = newNode;
+}
+
+void deleteFirstNode() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void printList() {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
