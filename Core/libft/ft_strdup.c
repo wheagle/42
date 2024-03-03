86d86b0e89da8d8ef179cc3b5ae08707de979448
Leonard French <ljfrench@gmail.com>
2024-03-03 20:51:00 +0100

    Started on strdup

---
+++ Core/libft/ft_strdup.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrench <lfrench@student.42luxembourg.lu>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 20:45:07 by lfrench           #+#    #+#             */
+/*   Updated: 2024/03/03 20:47:41 by lfrench          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* The  strdup() function returns a pointer to a new string which is a 
+   duplicate of the string s.  
+   Memory for the new string is obtained with malloc(3), 
+   and can be freed with free(3). 
+   On success, the strdup() function returns a pointer to the duplicated string.
+   It returns NULL if insufficient memory was available, with errno set to 
+   indicate the cause of the error. */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+}
--- Core/libft/libft.h
+++ Core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:37:28 by lfrench           #+#    #+#             */
-/*   Updated: 2024/03/03 20:11:26 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/03/03 20:44:57 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 size_t	ft_strlcat(char *dest, char *src, unsigned int size);
 size_t	ft_strlcpy(char *dest, char *src, unsigned int size);
