b6eec622e0a8e970a9259f377d2d03cef65bc0f2
Leonard French <ljfrench@gmail.com>
2024-03-03 19:32:55 +0100

    memcmp added and passes francinette. Break for now.

--- Core/libft/libft.h
+++ Core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lfrench <lfrench@student.42luxembourg      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:37:28 by lfrench           #+#    #+#             */
-/*   Updated: 2024/03/03 18:58:49 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/03/03 19:18:06 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
