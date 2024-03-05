0b1dcec62666ed8a7d752cb3836bfb77a2aeee10
Leonard French <lfrench@c2r10s3.42luxembourg.lu>
2024-03-05 14:12:54 +0100

    ft_split.c finished, passes libft-unit-tests

--- Core/libft/ft_split.c
+++ Core/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lfrench <lfrench@student.42luxembourg.lu>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 20:58:36 by lfrench           #+#    #+#             */
-/*   Updated: 2024/03/05 13:34:01 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/03/05 14:11:26 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,33 @@
 #include "libft.h"
 
 size_t	ft_count_words(char const *s, char c);
+char	*ft_next_word(char **s, char c);
 
 char	**ft_split(char const *s, char c)
 {
 	char	**strs;
-	char	*s_new;
 	size_t	word_count;
+	size_t	i;
 
+	if (s == NULL)
+		return (NULL);
 	word_count = ft_count_words(s, c);
 	strs = (char **)malloc(sizeof(char *) * (word_count + 1));
 	if (strs == NULL)
 		return (NULL);
-
-
-
+	i = 0;
+	while (i < word_count)
+	{
+		strs[i] = ft_next_word((char **)&s, c);
+		if (strs[i] == NULL)
+		{
+			while (i-- > 0)
+				free(strs[i]);
+			free(strs);
+			return (NULL);
+		}
+		i++;
+	}
 	strs[word_count] = NULL;
 	return (strs);
 }
@@ -45,8 +58,6 @@ size_t	ft_count_words(char const *s, char c)
 
 	word_count = 0;
 	is_word = 0;
-	//Step through *str and count the words
-	//Check for repeating c
 	while (*s)
 	{
 		if (*s != c && is_word == 0)
@@ -59,4 +70,18 @@ size_t	ft_count_words(char const *s, char c)
 		s++;
 	}
 	return (word_count);
+}
+
+char	*ft_next_word(char **s, char c)
+{
+	char	*word_start;
+	size_t	word_length;
+
+	while (**s && **s == c)
+		(*s)++;
+	word_start = *s;
+	while (**s && **s != c)
+		(*s)++;
+	word_length = *s - word_start;
+	return (ft_substr(word_start, 0, word_length));
 }
