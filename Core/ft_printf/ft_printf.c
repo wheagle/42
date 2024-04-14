bc7bb34508c3775e181c302068fa9015552126e3
Leonard French <ljfrench@gmail.com>
2024-04-14 12:37:37 +0200

    Beginning of ft_printf. .h created and .c begun. Disappointed that I can't overload my formatting prototypes.

--- Core/ft_printf/ft_printf.c
+++ Core/ft_printf/ft_printf.c
@@ -6,7 +6,18 @@
 /*   By: lfrench <lfrench@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:24:31 by lfrench           #+#    #+#             */
-/*   Updated: 2024/04/13 15:24:32 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/04/14 12:30:19 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	va_start(ap, format);
+	//find the number of arguments by the % specifiers in format
+	//start printing from format, interrupting at valid % specifiers
+	//for each % specifier in format, call its formatting function
+	//print the result
+	//loop until finished
+	va_end(ap);
+}
--- Core/ft_printf/ft_printf.h
+++ Core/ft_printf/ft_printf.h
@@ -6,7 +6,26 @@
 /*   By: lfrench <lfrench@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:24:34 by lfrench           #+#    #+#             */
-/*   Updated: 2024/04/13 15:24:35 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/04/14 12:22:37 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include "../libft/libft.a"
+
+int		ft_printf(const char *format, ...);
+char	*ft_c_to_str(char *c_input);
+char	*ft_s_to_str(char *s_input);
+char	*ft_p_to_str(char *p_input);
+char	*ft_d_to_str(char *d_input);
+char	*ft_i_to_str(char *i_input);
+char	*ft_u_to_str(char *i_input);
+char	*ft_x_to_str(char *x_input);
+char	*ft_X_to_str(char *X_input);
+char	*ft_perc_to_str(char *perc_input);
+
+#endif
