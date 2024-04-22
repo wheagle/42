183d13e40e6067fd6c44c5a1959782529a6aa6f9
Leonard French <lfrench@c2r10s5.42luxembourg.lu>
2024-04-22 12:32:41 +0200

    Basic makefile

--- Core/push_swap/push_swap.h
+++ Core/push_swap/push_swap.h
@@ -6,14 +6,20 @@
 /*   By: lfrench <lfrench@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:17:07 by lfrench           #+#    #+#             */
-/*   Updated: 2024/04/22 10:18:20 by lfrench          ###   ########.fr       */
+/*   Updated: 2024/04/22 12:28:07 by lfrench          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdlib.h>
+# include <stdlib.h>
+
+typedef struct s_stack
+{
+	int				data;
+	struct s_stack*	next;
+} t_stack;
 
 
 #endif
